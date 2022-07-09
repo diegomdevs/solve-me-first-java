acb647ac8a7874744e8d883146f65612a4743fe2
Diego <diegom.develops@gmail.com>
2022-07-08 23:32:59 -0400

    feat: SolveMeFirst
    Problem Solved :)

---
+++ solve-me-first/src/SolveMeFirst.java
@@ -0,0 +1,9 @@
+public class SolveMeFirst {
+    static short solveMeFirst(short a, short b) {
+        return (short) (a + b);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solveMeFirst((short) (500), (short) (500)));
+    }
+}
